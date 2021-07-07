f8fadcd100430d4d1eb7a863803d964164e1d9df
Vinu K <kevy.vinu@gmail.com>
2021-07-07 11:47:46 +0530

    added changes

--- cluster-files/install-config.sh
+++ cluster-files/install-config.sh
@@ -2,14 +2,14 @@
 # Populate install-config.yaml file using the ENV
 
 function SSH_CHECK {
-	if ! [[ -f ../downloads/id_ed25519.pub ]]; then
+	if ! [[ -f ${BASE_DIR}/downloads/id_ed25519.pub ]]; then
 		echo "SSH public key is not present in the downloads directory. Please run sshAndPullsecret.sh"
 		exit 1
 	fi
 }
 
 function PULL_SECRET_CHECK {
-	if ! [[ -f ../downloads/pull-secret ]]; then
+	if ! [[ -f ${BASE_DIR}/downloads/pull-secret ]]; then
 		echo "Pull secret is not present in the downloads directory. Please run sshAndPullsecret.sh"
 		exit 1
 	fi
@@ -48,9 +48,8 @@ cp install-config.yaml install-config.yaml.bkp
 SSH_CHECK
 PULL_SECRET_CHECK
 
-DOMAIN=example.com
-CLUSTER_NAME=ocp
-PULLSECRET=$(cat ../downloads/pull-secret)
-SSHKEY=$(cat ../downloads/id_ed25519.pub)
+source ../env
+PULLSECRET=$(cat ${BASE_DIR}/downloads/pull-secret)
+SSHKEY=$(cat ${BASE_DIR}/downloads/id_ed25519.pub)
 
 POPULATE_INSTALL_CONFIG
